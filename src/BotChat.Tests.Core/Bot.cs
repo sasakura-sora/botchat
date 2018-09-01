d17916eb4bdf2a62061cdcfaf607bfeddac5bb23
sasakura-sora <h34dphones@gmail.com>
2018-09-01 10:30:33 +0100

    Tests - Setting up first test project

---
+++ src/BotChat.Tests.Core/Bot.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace BotChat.Tests.Core
+{
+    [TestFixture]
+    public class Bot
+    {
+        [Test]
+        public void GivenTest_RunsInVS()
+        {
+            Assert.AreEqual(true, true);
+        }
+    }
+}
